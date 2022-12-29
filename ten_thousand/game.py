6639a345f909e4c33d45b2daf2d1be747995a7ee
dchoi294 <dchoi294@gmail.com>
2022-12-29 15:58:34 -0800

    some fixes

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,13 +1,17 @@
 from game_logic import GameLogic
 
+on_off = True
+
 
 def intro():
-    print("10000 GAME, wanna roll some dices?")
-    print("Press 'y' to start or 'n' to quit!")
+    print("Welcome to Ten Thousand")
+    print("(y)es to play or (n)o to decline")
     user = input("> ")
 
-    if user == "y":
-        print("Let's begin!")
+    if user == "n":
+        global on_off
+        on_off = False
+        print("OK. Maybe another time")
     else:
         exit()
 
@@ -19,10 +23,9 @@ def play_dice():
     current_dice = []
     dice_left = 6
     winning_score = 10000
-    on_off = True
 
     while on_off:
-        print("Press 'r' to roll the dices, 'b' to bank score and 'q' to quit.")
+
         user = input("> ")
         if user_score >= winning_score:
             print("You won!!")
@@ -35,22 +38,34 @@ def play_dice():
         if user == "r":
             roll = GameLogic.roll_dice(dice_left)
             score = GameLogic.calculate_score(roll)
-            print(f"Current Round: {round_numb}")
-            print(f"Current score: {user_score}")
-            print(f"Rolling {dice_left-len(current_dice)} dices...")
-            round_numb += 1
+            print(f"Starting round {round_numb}")
+            print(f"Rolling {dice_left-len(current_dice)} dice...")
+            print(f"*** {current_dice} ***")
 
         if score == 0:
             round_score = 0
             print("Farkled!")
         else:
-            round_score += roll
-            print(f"Banking points: {round_score}")
-            print("Roll again? (r), Bank score? (b), Quit? (q)")
+            print("Enter dice to keep, or (q)uit:")
 
-        if user == "b":
-            score += round_score
-            round_score = 0
+            user = input("> ")
+            if user == "q":
+                print(f"Thanks for playing. You earned {score} points")
+                break
+            for dice in user:
+                current_dice.append(int(dice))
+
+            round_score += roll
+            print(f"You have {round_score} unbanked points and {6 - len(current_dice)} dice remaining")
+            print(f"(r)oll again, (b)ank your points or (q)uit:")
+            user = input("> ")
+            if user == "b":
+                print(f"You banked {round_score} points in round {round_numb}")
+                score += round_score
+                print(f"Total score is {score} points")
+                round_score = 0
+                current_dice = []
+                round_numb += 1
 
 
 if __name__ == "__main__":
