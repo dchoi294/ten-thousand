ab37fd00a9a02b4a07dc781e7eb31fd36f195f26
dchoi294 <dchoi294@gmail.com>
2022-12-29 20:29:06 -0800

    progress

--- test/version_3/stretch_sim_advanced.py
+++ test/version_3/stretch_sim_advanced.py
@@ -1,5 +1,5 @@
 import pytest
-from tests.flo import diff
+from test.flo import diff
 from ten_thousand.game import play
 
 
@@ -9,7 +9,7 @@ pytestmark = [pytest.mark.version_3]
 def test_repeat_roller():
     """Allow setting aside scoring dice and rolling the rest
     """
-    diffs = diff(play, path="tests/version_3/repeat_roller.sim.txt")
+    diffs = diff(play, path="test/version_3/repeat_roller.sim.txt")
     assert not diffs, diffs
 
 
@@ -17,7 +17,7 @@ def test_hot_dice():
     """When all dice are used without a zilch
     then user gets 6 fresh dice and continues turn.
     """
-    diffs = diff(play, path="tests/version_3/hot_dice.sim.txt")
+    diffs = diff(play, path="test/version_3/hot_dice.sim.txt")
     assert not diffs, diffs
 
 
@@ -27,7 +27,7 @@ def test_cheat_and_fix():
     If invalid prompt user for re-entry
     """
 
-    diffs = diff(play, path="tests/version_3/cheat_and_fix.sim.txt")
+    diffs = diff(play, path="test/version_3/cheat_and_fix.sim.txt")
     assert not diffs, diffs
 
 
@@ -38,5 +38,5 @@ def test_zilcher():
     and ends turn
     """
 
-    diffs = diff(play, path="tests/version_3/zilcher.sim.txt")
+    diffs = diff(play, path="test/version_3/zilcher.sim.txt")
     assert not diffs, diffs
